a3d2cc995dfdd19ad1a09881d02622f368656c45
Muhammad Awaab <muhammad.awaab92@gmail.com>
2022-02-27 04:43:16 +0500

    network layer development complete.

---
+++ MovieApp/Services/MovieServices.swift
@@ -0,0 +1,49 @@
+//
+//  MovieServices.swift
+//  MovieApp
+//
+//  Created by Muhammad Oneeb on 27/02/2022.
+//
+
+import Foundation
+
+
+let dataFetchUrl = "https://api.themoviedb.org/3/discover/movie?api_key=c9856d0cb57c3f14bf75bdc6c063b8f3"
+
+let apiKey = "c9856d0cb57c3f14bf75bdc6c063b8f3"
+
+
+
+enum MovieServices {
+    case fetchMoviesListData
+    case movieDetail(movieID: String)
+}
+
+
+extension MovieServices: Service {
+    var baseURL: String {
+        return "https://api.themoviedb.org"
+    }
+    
+    var path: String {
+        switch self {
+        case .fetchMoviesListData:
+            return "/3/discover/movie"
+
+        case .movieDetail(let movieID):
+            return "3/movies/" + movieID
+        }
+    }
+    
+    var parameters: [String: Any]? {
+        // default params
+        var params: [String: Any] = [:]
+        params["api_key"] = apiKey
+        
+        return params
+    }
+    
+    var method: ServiceMethod {
+        return .get
+    }
+}
