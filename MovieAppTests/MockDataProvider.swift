62ccc6e629ca268157ab0caac956cb9b3be843bd
Muhammad Awaab <muhammad.awaab92@gmail.com>
2022-03-02 04:48:09 +0500

    Unit tests written for movies List.

---
+++ MovieAppTests/MockDataProvider.swift
@@ -0,0 +1,47 @@
+//
+//  MockDataProvider.swift
+//  MovieAppTests
+//
+//  Created by Muhammad Oneeb on 02/03/2022.
+//
+
+import Foundation
+
+
+class MockDataProvider: DataProviderProtocol {
+    func fetchAndProvideListData(completion: @escaping (MovieList?) -> Void) {
+        if let path = Bundle.main.path(forResource: "MovieAppSampleJson", ofType: "json") {
+                  do {
+                      let fileUrl = URL(fileURLWithPath: path)
+                      // Getting data from JSON file using the file URL
+                      let data = try Data(contentsOf: fileUrl, options: .mappedIfSafe)
+                      let jsonDecoder = JSONDecoder()
+                      let responseModel = try jsonDecoder.decode(MovieList.self, from: data)
+                      completion(responseModel)
+                  } catch {
+                      // Handle error here
+                      print("error parsing local json...")
+                      completion(nil)
+                  }
+              }
+    }
+    
+    func fetchMovieDetailWith(movieID: String, completion: @escaping (MovieDetail?) -> Void) {
+        if let path = Bundle.main.path(forResource: "MovieDetailSampleJson", ofType: "json") {
+                  do {
+                      let fileUrl = URL(fileURLWithPath: path)
+                      // Getting data from JSON file using the file URL
+                      let data = try Data(contentsOf: fileUrl, options: .mappedIfSafe)
+                      let jsonDecoder = JSONDecoder()
+                      let responseModel = try jsonDecoder.decode(MovieDetail.self, from: data)
+                      completion(responseModel)
+                  } catch {
+                      // Handle error here
+                      print("error parsing local json...")
+                      completion(nil)
+                  }
+              }
+    }
+    
+    
+}
--- MovieAppTests/MovieAppTests.swift
+++ MovieAppTests/MovieAppTests.swift
@@ -9,6 +9,9 @@ import XCTest
 @testable import MovieApp
 
 class MovieAppTests: XCTestCase {
+    
+    var mockProvider: DataProviderProtocol?
+    var listModelToTest: MainViewModelProtocol?
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -24,8 +27,25 @@ class MovieAppTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        listModelToTest?.fetchParsedDataForDisplay()
+        
+        let movieListObject = listModelToTest?.getDataDisplayed(at: IndexPath(row: 0, section: 0))
+        XCTAssertNotNil(movieListObject)
+
+        let movieListSecondObject = listModelToTest?.getDataDisplayed(at: IndexPath(row: 1, section: 0))
+        XCTAssertTrue(movieListSecondObject?.original_title == "Scream")
+        XCTAssertTrue(movieListSecondObject?.original_language == "en")
+        
+        
     }
 
+    
+    override func setUp() {
+        mockProvider = MockDataProvider()
+        listModelToTest = MainViewModel(provider: mockProvider!)
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
